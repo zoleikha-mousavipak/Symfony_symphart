a3995ffde4e1f20b5f0517ce1baae74c5dbfa3cd
zoleikha <zmpak2000@gmail.com>
2020-05-24 22:06:11 +0200

    annotation

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response("Helooooo");
+    }
+}
