d951256998278358cabde4e38e8aadecbf800798
zoleikha <zmpak2000@gmail.com>
2020-05-24 22:34:40 +0200

    annotation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
+     * @Method({"GET"})
      */
     public function index()
     {
-        return new Response("Helooooo");
+        // return new Response("Helooooo");
+        // using twig
+        return $this->render('articles/index.html.twig');
     }
 }
