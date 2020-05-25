527f307eac9712fa4ee7abf13eb4fe3f3240e0c4
zoleikha <zmpak2000@gmail.com>
2020-05-25 18:23:02 +0200

    new article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,11 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      */
     public function index()
@@ -27,14 +31,51 @@ class ArticleController extends AbstractController
         ]);
     }
 
+
+
+      /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET"}, {"POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+      $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')
+        ))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Create',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()) {
+        $article = $form->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('article_list');
+      }
+
+      return $this->render('articles/new.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+
       /**
      * @Route("/article/{id}", name="article_show")
      * @Method({"GET"})
      */
     public function show($id)
     {
-        // return new Response("Helooooo");
-        // using twig
 
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
@@ -42,6 +83,7 @@ class ArticleController extends AbstractController
             'article' => $article,
         ]);
     }
+
 
 
     // /**
