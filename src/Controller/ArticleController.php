58fa6a2e363f61b3ff5e6d70c12806e74f8bec7e
zoleikha <zmpak2000@gmail.com>
2020-05-25 18:55:04 +0200

    edit-delete article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -33,13 +33,68 @@ class ArticleController extends AbstractController
 
 
 
-      /**
+    /**
      * @Route("/article/new", name="new_article")
      * @Method({"GET"}, {"POST"})
      */
     public function new(Request $request)
     {
         $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+
+    /**
+     * @Route("/article/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
+    }
+
+ /**
+     * @Route("/article/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+      $article = new Article();
+      $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
       $form = $this->createFormBuilder($article)
         ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
@@ -48,7 +103,7 @@ class ArticleController extends AbstractController
           'attr' => array('class' => 'form-control')
         ))
         ->add('save', SubmitType::class, array(
-          'label' => 'Create',
+          'label' => 'Update',
           'attr' => array('class' => 'btn btn-primary mt-3')
         ))
         ->getForm();
@@ -56,21 +111,19 @@ class ArticleController extends AbstractController
       $form->handleRequest($request);
 
       if($form->isSubmitted() && $form->isValid()) {
-        $article = $form->getData();
 
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($article);
         $entityManager->flush();
 
         return $this->redirectToRoute('article_list');
       }
 
-      return $this->render('articles/new.html.twig', array(
+      return $this->render('articles/edit.html.twig', array(
         'form' => $form->createView()
       ));
     }
 
-      /**
+    /**
      * @Route("/article/{id}", name="article_show")
      * @Method({"GET"})
      */
