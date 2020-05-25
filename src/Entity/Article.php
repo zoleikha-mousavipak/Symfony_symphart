7a284540de7bc76ebced521e0ee1632ff2086a7c
zoleikha <zmpak2000@gmail.com>
2020-05-25 16:59:43 +0200

    Entity-Model-Config

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -17,8 +17,41 @@ class Article
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+        return $this;
     }
 }
